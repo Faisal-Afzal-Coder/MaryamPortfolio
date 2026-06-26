fa49b9236ca1ef05a7dafbf453ba6cfede6cc2d0
Faisal Afzal <educatedboy610@gmail.com>
2026-06-26 18:09:00 +0000

    URl change in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://maryamportfolio1234.vercel.app";
+  const baseUrl = "https://maryam-portfolio-uawd.vercel.app";
 
   return [
     {
